fe2ab585e86796541b701cadf1a7b23ab35b1931
essey1 <esseyt3@gmail.com>
2023-11-14 21:58:31 +0300

    styled the nav-bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,23 +9,22 @@ interface ShoppingCart {
 export function Navbar() {
     const { openCart, cartQuantity } = useShoppingCart() as ShoppingCart
     return <div className="nav">
-        <div className="nav__logo">
-            <img src="img/logo.png" alt="logo" />
-        </div>
-        <button className="nav__toggle" aria-label="toggle navigation">
-            <span className="hamburger"></span>
-        </button>
-        {cartQuantity > 0 &&(
-            <div className="nav__cart" onClick={openCart}>
-                Cart
-                <img src="#" alt="" />
-                <div className="nav__cart--circle">{cartQuantity}</div>
-            </div>
-        )}
-        <nav className="nav">
+        {/* <div className="nav__logo">
+            <img src="img/favicon.png" alt="logo" />
+        </div> */}
+        <nav className="nav__list">
             <NavLink to={"/"} className="nav__link">Home</NavLink>
             <NavLink to={"/store"} className="nav__link">Store</NavLink>
             <NavLink to={"/about"} className="nav__link">About</NavLink>
         </nav>
+        {/* <button className="nav__toggle" aria-label="toggle navigation">
+            <span className="hamburger"></span>
+        </button> */}
+        {cartQuantity > 0 &&(
+            <div className="nav__cart" onClick={openCart}>
+                <img src="img/cart.png" alt="" />
+                <div className="nav__cart--circle">{cartQuantity}</div>
+            </div>
+        )}
     </div>
 }
