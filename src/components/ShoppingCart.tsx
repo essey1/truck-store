46061d060b6adfd7db31a828e0786674c9370db2
essey1 <esseyt3@gmail.com>
2023-12-06 18:53:20 +0300

    minor changes

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,4 +1,4 @@
-import { Offcanvas } from "react-bootstrap"
+import { Offcanvas, Stack } from "react-bootstrap"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import storeItems from "../data/items.json"
 import { CartItem } from "./CartItem"
@@ -26,18 +26,20 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
                 <Offcanvas.Title>Cart</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                {cartItems.map(item => (
-                <CartItem key={item.id} {...item} />
-                ))}
-                <div className="cart__totalp">
-                    Total: 
-                    {formatCurrency(
-                        cartItems.reduce((total, cartItem) =>{
+                <Stack gap={3}>
+                    {cartItems.map(item => (
+                        <CartItem key={item.id} {...item} />
+                    ))}
+                    <div className="ms-auto fw-bold fs-5">
+                        Total{" "}
+                        {formatCurrency(
+                        cartItems.reduce((total, cartItem) => {
                             const item = storeItems.find(i => i.id === cartItem.id)
                             return total + (item?.price || 0) * cartItem.quantity
                         }, 0)
-                    )}
-                </div>
+                        )}
+                        </div>
+                </Stack>
             </Offcanvas.Body>
         </Offcanvas>
     )
