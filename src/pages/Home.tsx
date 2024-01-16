b05ebe14b24cbb43a0bf66c8518ee439d21e25c5
essey1 <esseyt3@gmail.com>
2024-01-16 20:21:16 +0300

    testing whether there is a problem with the local server not displaying the video

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,14 @@
 export function Home() {
-    return <h1>Home</h1>
+    return <>
+        <section className="home" id="hero">
+            <h1 className="section__title section__title--home">
+                Truck Store
+            </h1>
+            <p className="section__subtitle section__subtitle--home">Moving you Forward, Mile by Mile</p>
+            <video className="video-bg" 
+                autoPlay muted loop controls>
+                <source src="1.mp4" type="video/mp4"></source>
+            </video>
+        </section>
+    </>
 }
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -3,7 +3,7 @@ import storeItems from "../data/items.json"
 
 export function Store() {
     return <section>
-        <h1 className="section__title">Toughness Meets Reliability in Every Truck!</h1>
+        <h1 className="section__title section__title--store">Toughness Meets Reliability in Every Truck!</h1>
         <div className="card-container">
             {storeItems.map(item => (
                 <div key={item.id}>
