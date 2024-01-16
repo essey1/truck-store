716b17bbc362fc7adc60f157ac6e2ac2968e28f8
essey1 <esseyt3@gmail.com>
2024-01-16 22:05:54 +0300

    minor changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
 export function Home() {
     return <>
         <section className="home" id="hero">
-            <h1 className="section__title section__title--home">
-                Truck Store
-            </h1>
-            <p className="section__subtitle section__subtitle--home">Moving you Forward, Mile by Mile</p>
-            <video autoPlay muted loop>
+            <div className="home__titles">
+                <h1 className="section__title section__title--home">
+                    Truck Store
+                </h1>
+                <p className="section__subtitle section__subtitle--home">Moving you Forward, Mile by Mile</p>
+
+            </div>
+            
+            <video className="home__video" autoPlay muted loop>
                 <source src="img/TRuck.webm" type="video/webm" />
                 Sorry, your browser doesn't support videos.
             </video>
